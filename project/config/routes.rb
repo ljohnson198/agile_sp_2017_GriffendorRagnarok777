11853756f6e4d84cd69003162eda8b22bfbd6ec8
Chelsey Fay <chelseyfay1810@gmail.com>
2017-09-24 02:48:53 -0500

    Commited changes to navigation

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  
+
 resources :user_addresses
 resources :user_profiles
 resources :users
 
   root controller: 'home', action: 'index'
 
-
+get "/ride", controller: 'home', action: 'ride', action: 'ride'
+get "/drive", controller: 'home', action: 'drive', action: 'drive'
+get "/signin", controller: 'home', action: 'signin'
+get "/signup", controller: 'home', action: 'signup'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

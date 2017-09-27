071594c23ec7285e32dd02e66a485cc1f96fef2c
Jason Fellows <jfellows@student.cvtc.edu>
2017-09-27 18:25:50 -0400

    Deleted profile and addresses db for rebuild

--- project/app/models/user_address.rb
+++
@@ -1,4 +0,0 @@
-class UserAddress < ActiveRecord::Base
-      validates_presence_of :userName, :streetAddress, :City, :State, :Zip
-      validates :userName, presence: true, uniqueness: true
-end
--- project/app/models/user_profile.rb
+++
@@ -1,4 +0,0 @@
-class UserProfile < ActiveRecord::Base
-    validates_presence_of :userName, :gender
-    validates :userName, presence: true, uniqueness: true
-end
